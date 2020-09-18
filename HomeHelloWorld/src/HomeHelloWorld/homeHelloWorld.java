5994fa0eb316f7d17c31b1d54a4324e49f28f0e0
SiriShortcutboi <58081965+SiriShortcutboi@users.noreply.github.com>
2020-09-18 11:23:22 -0600

    Add files via upload

---
+++ HomeHelloWorld/src/HomeHelloWorld/homeHelloWorld.java
@@ -0,0 +1,13 @@
+package HomeHelloWorld;
+
+public class homeHelloWorld {
+
+	public homeHelloWorld() {
+		System.out.println("Hello World");
+	}
+
+	public static void main(String[] args) {
+		new homeHelloWorld();
+	}
+
+}
